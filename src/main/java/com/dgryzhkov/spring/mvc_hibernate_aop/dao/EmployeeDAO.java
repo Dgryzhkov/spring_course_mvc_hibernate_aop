dfd10ae26ed745b3defe614609eacea04bd218d4
Denis Ryzhkov <dgryzhkov@gmail.com>
2022-09-13 00:51:19 +0700

    list Employee

---
+++ src/main/java/com/dgryzhkov/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -0,0 +1,12 @@
+package com.dgryzhkov.spring.mvc_hibernate_aop.dao;
+
+import com.dgryzhkov.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+/**
+ * @author Dgryzhkov
+ */
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+}
